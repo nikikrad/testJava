2d84bd71db076aab9e0f534b29c75b51ea8ef979
NiKiKRaD <nikikrad228@gmail.com>
2021-09-27 16:47:33 +0300

    Created Scelet.java

---
+++ src/Calculator/Main.java
@@ -0,0 +1,11 @@
+package Calculator;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scelet app = new Scelet();
+        app.setVisible(true);
+
+    }
+}
---
+++ src/Calculator/Scelet.java
@@ -0,0 +1,26 @@
+package Calculator;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Scelet extends JFrame{
+
+    private JButton button = new JButton("Press");
+    private JLabel lable = new JLabel("input");
+    private JTextField input  = new JTextField("",5);
+    private JCheckBox chack = new JCheckBox();
+
+    public Scelet(){
+        JFrame frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        frame.setSize(320,240);
+
+
+        Container container = this.getContentPane();
+        container.setLayout(new GridLayout(3,2,2,2));
+
+        container.add(input);
+        container.add(lable);
+    }
+}
